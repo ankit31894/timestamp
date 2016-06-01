5ec659bf0a6bee72bc92c8c3ecb3ff20a0919118
Ankit <ankit31894@gmail.com>
2016-06-01 20:52:02 +0530

    F

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-'use strict';
 
-var express = require('express');
+var express = require("express");
+var http = require("http");
 var app = express();
 
+app.all("*", function(request, response, next) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  next();
+});
 
+app.get("/", function(request, response) {
+  response.end("Welcome to the homepage!");
+});
 
+app.get("/about", function(request, response) {
+  response.end("Welcome to the about page!");
+});
+
+app.get("*", function(request, response) {
+  response.end("404!");
+});
+
+http.createServer(app);
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
