3d3aecc5428f9cd111c8b1f8568e73fa1750808c
Ankit <ankit31894@gmail.com>
2016-06-01 21:18:23 +0530

    F

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 
 var express = require("express");
 var http = require("http");
+var moment = require('moment');
+moment().format();
 var app = express();
 
 app.all("*", function(request, response, next) {
   response.writeHead(200, { "Content-Type": "text/plain" });
   next();
 });
-
-app.get("/", function(request, response) {
-  response.end("Welcome to the homepage!");
-});
-
-app.get("/about", function(request, response) {
-  response.end("Welcome to the about page!");
-});
-
 app.get("*", function(request, response) {
-  response.end("404!");
+    var momen=moment();
+    response.write(request);
+  response.end(JSON.stringify({unix:momen.unix(),natural:momen.format('MMMM Do, YYYY)}));
 });
 
 http.createServer(app);
