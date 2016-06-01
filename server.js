c93ffca41066fbf1e8605655b789f2caf9ccc505
Ankit <ankit31894@gmail.com>
2016-06-01 20:23:12 +0530

    F

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+
+
+var port = process.env.PORT || 8080;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
